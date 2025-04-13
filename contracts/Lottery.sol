aaef673eb208895d1d681a95d54f1a697f903744
Pacfist <knyazevmatvey4@gmail.com>
2025-04-12 20:32:39 -0400

    made ownable

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,18 +2,26 @@
 pragma solidity ^0.8.8;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
-contract Lottery {
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract Lottery is Ownable {
     address payable[] public players;
     int256 public usdEntryFee;
     AggregatorV3Interface internal dataFeed;
-    constructor() public {
+    enum LOTTERY_STATE{
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+    constructor() Ownable(msg.sender){
         // usdEntryFee = 50 * (10 ** 18);
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
     function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(payable(msg.sender));
     }
@@ -28,8 +36,13 @@ contract Lottery {
         uint256 costToEnter = (50 * 10**18) / uint256(answer) * 10**9;
         return costToEnter;
     }
-    function startLottery() public {}
-    function endLottery() public {}
+    function startLottery() public onlyOwner{
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet");
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+    function endLottery() public onlyOwner {
+        
+    }
 
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
         // prettier-ignore
