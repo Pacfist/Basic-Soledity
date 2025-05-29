1221d046b0d29e37b0c1506e53daaab8d6bbeea4
Pacfist <knyazevmatvey4@gmail.com>
2025-05-29 20:46:32 +0500

    123

--- contracts/basics.sol
+++ contracts/basics.sol
@@ -2,15 +2,43 @@
 pragma solidity ^0.8.8;
 
 contract Basics {
-    bool public isGood = false;
+    
 
-    function amIPoor() public returns(bool) {
-        if(msg.sender.balance > 10 ether){
-            isGood = true;
-        }else {
-            isGood = false;
-        }
+    // bytes8 public myBytes = "soledity";
 
-        return isGood;
+    // function getBytesByIndex(uint256 _index) public view returns (bytes1) {
+    //     return myBytes[_index];
+    // }
+
+    // enum Skills {NOOB, ADVANCED, PRO}
+
+    // Skills public someSKill;
+
+    // function setSkill() public {
+    //     someSKill = Skills.ADVANCED;
+    // }
+
+    mapping(address => uint256) public userBalances;
+
+    function addBalance(address _user, uint256 _balance) public {
+        userBalances[_user] =_balance;
+    } 
+
+    mapping(address => string) public nameAddress;
+
+    function addName(address _add, string memory _name) public {
+        nameAddress[_add] = _name;
+    }
+
+    function getBalance(address _add) public view returns(uint256) {
+        return userBalances[_add];
+    }
+
+    function deposit() public payable {
+        userBalances[msg.sender] += msg.value;
+    }
+    address public owner;
+    constructor() {
+        owner = msg.sender;
     }
 }
